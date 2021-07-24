901889e25ef0b5ad000dd458ead36bec7886dfee
Soufiane CHALOUH <schalouh@leyton.com>
2021-07-24 13:02:42 +0100

    initiate flask restx

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
+from flask_restx import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+@api.route('/hello')
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
